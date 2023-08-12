f08479172599b2ab1ec3ea29f44fafa39e18eb35
reesericci <me@reeseric.ci>
2023-08-12 23:26:21 +0000

    Add production DNSimple endpoint, if production env

--- app/helpers/dnsimple_helper.rb
+++ app/helpers/dnsimple_helper.rb
@@ -1,6 +1,6 @@
 module DnsimpleHelper
   @@client = Dnsimple::Client.new(
-      base_url: "https://api.sandbox.dnsimple.com",
+    base_url: if Rails.env.production? then "https://api.dnsimple.com" else "https://api.sandbox.dnsimple.com" end,
       access_token: Rails.application.credentials.dnsimple.access_token
     )
   def client
