d51221f876723bc562446020ca4dec2095b1add9
reesericci <me@reeseric.ci>
2023-09-18 10:26:58 -0500

    fix a diff error x3

--- app/mailers/domain_mailer.rb
+++ app/mailers/domain_mailer.rb
@@ -4,6 +4,6 @@ class DomainMailer < ApplicationMailer
   def domain_created_email
     @user = params[:user]
     @domain = params[:domain]
-    mail(to: user.email, subject: "Your domain has been created!", message_stream: 'outbound')
+    mail(to: @user.email, subject: "Your domain has been created!", message_stream: 'outbound')
   end
 end
