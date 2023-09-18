d9dc89d7e21da9f4e5ad8f6faa05c5c30c6852bb
reesericci <me@reeseric.ci>
2023-09-18 09:58:37 -0500

    update callbacks

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -14,6 +14,11 @@ class Domain < ApplicationRecord
     Record.destroy_all_host!(host)
   end
 
+  after_update, :if => :user_users_changed? do 
+    DomainMailer.with(user: User.find_by(id: user_users_id), domain: self).domain_created_email.deliver_later
+     
+  end
+
   def to_param
     host
   end
