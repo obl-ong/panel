761927deb0e156e195f262c67c93362d9b71735f
reesericci <me@reeseric.ci>
2024-02-23 08:16:44 +0000

    Add CSP and nonces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def style_tag(content_or_options_with_block = nil, html_options = {}, &block)
+    content =
+      if block_given?
+        html_options = content_or_options_with_block if content_or_options_with_block.is_a?(Hash)
+        capture(&block)
+      else
+        content_or_options_with_block
+      end
+
+    if html_options[:nonce] == true
+      html_options[:nonce] = content_security_policy_nonce
+    end
+
+    content_tag("style", content.html_safe, html_options)
+  end
 end
