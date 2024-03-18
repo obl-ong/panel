6effc0443db862dec43eba60580a4b8d330b2925
reesericci <me@reeseric.ci>
2024-03-18 05:48:04 +0000

    Sentry SDK & session replays

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,13 +6,20 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.default_src :self, "https://rebound.postmarkapp.com", "https://esm.sh/v135/selectlist-polyfill@0.3.0/es2022/selectlist-polyfill.mjs", "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js", "https://esm.sh/selectlist-polyfill@0.3.0"
+    policy.default_src :none 
+    policy.connect_src :self, "https://rebound.postmarkapp.com/check", "https://*.sentry.io/"
     policy.font_src :self, :data
     policy.img_src :self, :data
     policy.object_src :none
-    policy.script_src :self, "https://rebound.postmarkapp.com", "https://code.ionicframework.com", "https://cdn.jsdelivr.net/npm/toastify-js"
+    policy.script_src :self, "'strict-dynamic'", "https://code.ionicframework.com", "https://cdn.jsdelivr.net/npm/toastify-js", "https://ga.jspm.io/npm:@sentry/", "https://ga.jspm.io/npm:@sentry-internal/", "https://rebound.postmarkapp.com/", "https://esm.sh/v135/selectlist-polyfill@0.3.0/", "https://ga.jspm.io/npm:local-time@3.0.2/", "https://esm.sh/selectlist-polyfill@0.3.0/"
     policy.style_src :self, "https://unpkg.com/cursor-chat/dist/style.css", "https://code.ionicframework.com", "https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css"
     policy.style_src_attr :self, "'unsafe-inline'"
+    policy.script_src_attr :self, "'unsafe-inline'"
+    policy.frame_ancestors :self
+    policy.base_uri :self
+    policy.form_action :self
+    policy.worker_src :self, "blob:"
+    policy.child_src :self, "blob:"
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
